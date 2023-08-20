c29e26c7916840746b24e9b619003f15fe6bc4bf
Devlee <hjcloud@me.com>
2023-08-20 13:37:31 +0900

    자바가 제공하는 함수형 인터페이스

--- java8/src/main/java/study/java/java8/Foo.java
+++ java8/src/main/java/study/java/java8/Foo.java
@@ -1,11 +1,34 @@
 package study.java.java8;
 
+import java.util.function.*;
+
 public class Foo {
 
     public static void main(String[] args) {
-        RunSomething runSomething = () -> System.out.println("Hello");
-        runSomething.doIt();
-        runSomething.printAge();
-        RunSomething.printName();
+
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+
+        System.out.println(multiply2.compose(plus10).apply(2));
+        System.out.println("andThen.apply(2) = " + plus10.andThen(multiply2).apply(2));
+
+        BiFunction<Integer, String, String> printAge = (i, s) -> i + s;
+        System.out.println("printAge.apply(3, 살) = " + printAge.apply(3, "살"));
+
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println("get10.get() = " + get10.get());
+
+        Predicate<String> startWithDev = (s) -> s.startsWith("dev");
+        System.out.println(startWithDev.test("devlee"));
+        System.out.println(startWithDev.negate().test("devlee"));
+
+        UnaryOperator<Integer> plus10Unary = (i) -> i + 10;
+        System.out.println(plus10Unary.apply(2));
+
+        BinaryOperator<Integer> multiply = (i, j) -> i * j;
+        System.out.println(multiply.apply(2, 8));
     }
 }
--- java8/src/main/java/study/java/java8/RunSomething.java
+++ java8/src/main/java/study/java/java8/RunSomething.java
@@ -1,18 +1,16 @@
 package study.java.java8;
 
-/**
- * 함수형 인터페이스 정의 시 견고한 관리 가능
- */
-@FunctionalInterface
+
+@FunctionalInterface // 함수형 인터페이스 정의 시 견고한 관리 가능
 public interface RunSomething {
 
     void doIt();
 
     static void printName() {
-        System.out.println("HyunJae");
+        System.out.println("Devlee");
     }
 
     default void printAge() {
-        System.out.println("32");
+        System.out.println("3");
     }
 }
