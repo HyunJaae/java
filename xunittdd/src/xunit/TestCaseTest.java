752a520e80817734228c73d3de5e12e311ed3732
Devlee <hjcloud@me.com>
2023-09-25 22:03:05 +0900

    create test framework

--- xunittdd/src/xunit/TestCaseTest.java
+++ xunittdd/src/xunit/TestCaseTest.java
@@ -1,2 +1,26 @@
-package xunit;public class TestCaseTest {
+package xunit;
+
+public class TestCaseTest extends TestCase {
+    public TestCaseTest(String name) {
+        super(name);
+    }
+
+    WasRun wasRun;
+
+    @Override
+    public void setUp() {
+        wasRun = new WasRun("testMethod");
+    }
+
+    public void testRunning() {
+        Assert.assertEquals(false, wasRun.wasRun);
+        wasRun.run();
+        Assert.assertEquals(true, wasRun.wasRun);
+    }
+
+    public void testSetUp() {
+        Assert.assertEquals(false, wasRun.wasSetUp);
+        wasRun.run();
+        Assert.assertEquals(true, wasRun.wasSetUp);
+    }
 }
--- xunittdd/src/xunit/WasRun.java
+++ xunittdd/src/xunit/WasRun.java
@@ -1,10 +1,13 @@
 package xunit;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class WasRun extends TestCase {
     public boolean wasRun;
+    public boolean wasSetUp;
+
+    @Override
+    public void setUp() {
+        wasSetUp = true;
+    }
 
     public WasRun(String name) {
         super(name);
