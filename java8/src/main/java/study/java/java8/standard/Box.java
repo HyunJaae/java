1620c401be692812e21761152a60f851cc0dea9f
Devlee <hjcloud@me.com>
2023-09-25 22:03:42 +0900

    good code, bad code test code

--- java8/src/main/java/study/java/java8/standard/Box.java
+++ java8/src/main/java/study/java/java8/standard/Box.java
@@ -1,2 +1,17 @@
-package study.java.java8.standard;public class Box {
+package study.java.java8.standard;
+
+/**
+ * Generic Class
+ * @param <T> Type variable
+ */
+public class Box<T> {
+
+    T item;
+
+    void setItem(T item) {
+        this.item = item;
+    }
+    T getItem() {
+        return item;
+    }
 }
--- java8/src/main/java/study/java/java8/standard/Temp.java
+++ java8/src/main/java/study/java/java8/standard/Temp.java
@@ -1,2 +1,11 @@
-package study.java.java8.standard;public class Temp {
+package study.java.java8.standard;
+
+public class Temp {
+
+    public static void main(String[] args) {
+        Box<String> box = new Box<>();
+        box.setItem("HyeonJae");
+
+        System.out.println(box.getItem());
+    }
 }
